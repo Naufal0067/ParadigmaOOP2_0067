f1c2dcd0c8f3fe9f4291a7ee2367067fc78ab68d
Naufal0067 <naufalopang1928@gmail.com>
2024-05-27 14:34:54 +0700

    class seseorang dan membuat pure function

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -1,2 +1,10 @@
 #include <Iostream>
 using namespace std;
+
+class seseorang {
+public:
+    virtual void pesan() = 0;
+    //virtual void() {
+        //cout<<"Pesan dari seseorang"<<endl;
+    //}
+};
